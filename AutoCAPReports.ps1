43f1a91a7d7caf47f4afd56de4c29a2a83f6dc0d
s4ndm4n <sadaruwa12@gmail.com>
2022-04-04 09:29:22 +0530

    Added email sending and file checkin

--- AutoCAPReports.ps1
+++ AutoCAPReports.ps1
@@ -3,7 +3,7 @@
    *                 AutoCAPReport                      *
    *                                                    *
    *  Created by: Sandaruwan Samaraweera                *
-   *  Version: 1.0.1                                    *
+   *  Version: 1.0.2                                    *
    *                                                    *
    ****************************************************** #>
 
@@ -47,6 +47,12 @@ $cmd = $cmd_command + " " + $dates + " " + $pm
 #>
 
 # Variable decleration.
+#Directory locations
+$WorkingDirectory = "E:\Tools\AutoCAPReports" # Directory location of the script. Please set this path if not script wont work corrrectly.
+
+# Active computer name.
+$ComputerName = [Environment]::MachineName
+
 # Dates
 $StartDate  = (Get-Date).AddDays(-1).ToString("dd.MM.yyyy")
 $EndDate    = (Get-Date).ToString("dd.MM.yyyy")
@@ -54,61 +60,188 @@ $DateForAuditLogFc   = $StartDate + " " + $EndDate
 $DateForTimeSpent    = $StartDate + " " + $StartDate
 
 # Email addresses
-$ToEmailAddressesList   = @("Sandaruwan Samaraweera<sandaruwan.s@pro-account.lk>","Heman Krishantha<heman.krishantha@pro-account.lk>","Priyantha Kumarasiri<priyantha.kumarasiri@pro-account.lk>","Sachini Madara<sachini.madara@pro-account.lk>","Nipun Karunarathna<nipun.karunarathna@pro-account.lk>")
+$ToEmailAddressesList   = @("Heman Krishantha<heman.krishantha@pro-account.lk>","Priyantha Kumarasiri<priyantha.kumarasiri@pro-account.lk>","Sachini Madara<sachini.madara@pro-account.lk>","Nipun Karunarathna<nipun.karunarathna@pro-account.lk>")
+$ErrorMailgList = @("Sandaruwan Samaraweera<sandaruwan.s@pro-account.lk>","Andreas Moen Arnet<andreas.moen.arnet@digitalcapture.no>","Fredrik Skjellum<fredrik.skjellum@digitalcapture.no>")
 
 # Executable path
 $McCommand  = '"C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport'
 
 # Parametter values
-$DataBaseAddress  = 'dmz-sql-01.dmz.local\pronav,59116'
-$DataBaseTable    = 'ATC_Archive'
-$DataBaseUser     = 'sa'
-$DataBasePassword = 'Asi@16'
+$DataBaseAddress  = '<Your database address>'
+$DataBaseTable    = '<The archive tabel>'
+$DataBaseUser     = '<Dabase username>'
+$DataBasePassword = '<Database password>'
 $ReportType       = '/type:EXCEL'
-$UsedFields       = '/fields:"Voucher Type,Payment Method,Currency,Voucher Date"'
+$UsedFields       = '/fields:"Voucher Type,Payment Method,Currentcy,Voucher Date,Due Date,Voucher Number,Bank Account,KID,Mva Amount,Total Invoice Amount Mva,Total Invoice Amount Excl Mva,Rounding Amount,Prepaid Amount,Supplier Organization No,Supplier Name,Supplier Address,Supplier City,Supplier Postal Code,Supplier Country,Customer Organization No,Customer Name,Customer Address,Customer City,Customer Country,Invoice Reference,Contract Document,Accounting Cost,Order Reference,Supplier Reference,Customer Reference,Delivery Date,Delivery Address,Delivery City,Delivery Postal Code,Delivery Country,Product No,Description,Mva,Unit,Unit Price,Quantity,Discount,Unit Price Incl Discount,Net Amount,Amount Incl Mva"'
 
 # Report names
 $AuditLogFieldChanges   = 'AuditLogFieldChanges'
 $TimeSpent              = 'TimeSpent'
 
 # Report output folder
-$OutPutFolder = "$($PWD)\Reports"
+$OutPutFolder = "$($WorkingDirectory)\Reports"
+
+# Log file
+$ToDaysDate = Get-Date -Format "dd-MM-yyyy"
+$LogFileName = "ACR_$($ToDaysDate).log"
+$LogFileSavePath = "$($WorkingDirectory)\Logs\$($LogFileName)"
+
+# Checks the log file
+if (!(Test-Path $LogFileSavePath)){
+   New-Item -ItemType File -Path $LogFileSavePath | Out-Null
+}
+
+#Functions
+function Write-Log($Message){
+   $TimeStamp = (Get-Date).ToString("HH:mm:ss")
+   "$($ToDaysDate)::$($TimeStamp)::$($Message)" >> $LogFileSavePath
+}
+
+function Get-AuditLogFcFileName{
+   #To get the file name with the extention.
+   $AuditLogFcFileNames    =  @(Get-ChildItem -Path $OutPutFolder | Where-Object {($_.CreationTime -gt (Get-Date).Date -and $_.Name -match "AuditLogFieldChanges")} | ForEach-Object -Process {[System.IO.Path]::GetFileName($_)})
+
+   if ($AuditLogFcFileNames){
+      # Count the filenames in the array.
+      $AuditLogFcFileNameCount = $AuditLogFcFileNames.count
+      
+      # Assignes the first file name if there's more than one similar file names in the array.
+      if ($AuditLogFcFileNameCount -gt 1){
+         $AuditLogFcFileName = $AuditLogFcFileNames[0]
+      } else {
+         $AuditLogFcFileName = $AuditLogFcFileNames
+      }
+      # Returns all the values. To be used by the file gen. and email sending codes.
+      return $AuditLogFcFileName, $AuditLogFcFileNames, $AuditLogFcFileNameCount
+   }   
+}
+
+function Get-TimeSpentFileName{
+   $TimeSpentFileNames     =  @(Get-ChildItem -Path $OutPutFolder | Where-Object {($_.CreationTime -gt (Get-Date).Date -and $_.Name -match "TimeSpent")} | ForEach-Object -Process {[System.IO.Path]::GetFileName($_)})
+
+   if ($TimeSpentFileNames){
+      # Count the filenames in the array.
+      $TimeSpentFileNameCount = $TimeSpentFileNames.count
+
+      # Assignes the first file name if there's more than one similar file names in the array.
+      if ($TimeSpentFileNameCount -gt 1){
+         $TimeSpentFileName = $TimeSpentFileNames[0]
+      } else {
+         $TimeSpentFileName = $TimeSpentFileNames
+      }
+      # Returns all the values. To be used by the file gen. and email sending codes.
+      return $TimeSpentFileName, $TimeSpentFileNames, $TimeSpentFileNameCount
+   }
+}
 
 # Comands to run
 # For AuditLogFieldChanges
 $CMD_AuditLogFc = $McCommand + " " + $AuditLogFieldChanges + " " + $DateForAuditLogFc + " " + $OutPutFolder + " " + $DataBaseAddress + " " + $DataBaseTable + " " + $DataBaseUser + " " + $DataBasePassword + " " + $ReportType + " " + $UsedFields
 
 # For TimeSpent
-$CMD_TimeSpent  = $McCommand + " " + $DateForTimeSpent + " " + $TimeSpent + " " + $OutPutFolder + " " + $DataBaseAddress + " " + $DataBaseTable + " " + $DataBaseUser + " " + $DataBasePassword + " " + $ReportType
+$CMD_TimeSpent  = $McCommand + " " + $TimeSpent + " " + $DateForTimeSpent + " " + $OutPutFolder + " " + $DataBaseAddress + " " + $DataBaseTable + " " + $DataBaseUser + " " + $DataBasePassword + " " + $ReportType
 
-# TODO: use the file name check to see if theres an file which got created to day. If yes just send the email no need to run the command.
 #To get the file name with out the extention.
 #$AuditLogFcFileNames  =   @(Get-ChildItem -Path $OutPutFolder | Where-Object {($_.CreationTime -gt (Get-Date).Date -and $_.Name -match "AuditLogFieldChanges")} | ForEach-Object -Process {[System.IO.Path]::GetFileNameWithoutExtension($_)})
 
-#To get the file name with the extention.
-$AuditLogFcFileNames  =   @(Get-ChildItem -Path $OutPutFolder | Where-Object {($_.CreationTime -gt (Get-Date).Date -and $_.Name -match "AuditLogFieldChanges")} | ForEach-Object -Process {[System.IO.Path]::GetFileName($_)})
+# Get the generated file details for AuditLogFieldChanges report.
+$AuditLogFcFileName, $AuditLogFcFileNames, $AuditLogFcFileNameCount = Get-AuditLogFcFileName
 
-$AuditLogFcFileNameCount = $AuditLogFcFileNames.count
+if ($AuditLogFcFileNameCount -gt 0) {
 
-if ($AuditLogFcFileNameCount -gt 1){
-   $AuditLogFcFileName = $AuditLogFcFileNames[0]
-}else {
-   $AuditLogFcFileName = $AuditLogFcFileNames
-}
+   Write-Log "File $($AuditLogFcFileName) already exists ... Starting to send emails."
 
-if ($AuditLogFcFileNameCount -gt 1) {
    # Send email with AuditLogFieldchanges report attached.
    foreach ($ToEmailAddress in $ToEmailAddressesList) {
-      Send-MailMessage -From "no-reply@navitro.com" -To "$($ToEmailAddress)" -Subject "$($AuditLogFieldChanges) for $($StartDate)" -Body "Generated $($AuditLogFieldChanges) report for $($StartDate)." -Attachment "$($PWD)\Reports\$($AuditLogFcFileName)" -SmtpServer mx01.minedata.no -Port 25
+      Send-MailMessage -From "no-reply@navitro.com" -To "$($ToEmailAddress)" -Subject "$($AuditLogFieldChanges) for $($StartDate) on $($ComputerName)" -Body "Generated $($AuditLogFieldChanges) report on $($ComputerName) for $($StartDate)." -Attachment "$($WorkingDirectory)\Reports\$($AuditLogFcFileName)" -SmtpServer mx01.minedata.no -Port 25
       if ($?){
-         Write-Host "Email sent ..."
+         Write-Log "Email sent to $($ToEmailAddress)..."
       }
-   } else {
-      # Run the ManagementConsole using commandprompt uses the above variables which makes the command.
-      cmd /c $CMD_AuditLogFc
+   }
+} else {
+   
+   Write-Log "File not found ... running command to generate the file."
+
+   # Run the ManagementConsole using commandprompt uses the above variables which makes the command.
+   cmd /c $CMD_AuditLogFc
+
+   if ($?){
+      #Get the generated file name.
+      $AuditLogFcFileName, $AuditLogFcFileNames, $AuditLogFcFileNameCount = Get-AuditLogFcFileName
+
+      if (Test-Path $WorkingDirectory\Reports\$AuditLogFcFileName){
+         Write-Log "File Generated ... $($AuditLogFcFileName)."
+         
+         # Send email with AuditLogFieldchanges report attached.
+         foreach ($ToEmailAddress in $ToEmailAddressesList) {
+            Send-MailMessage -From "no-reply@navitro.com" -To "$($ToEmailAddress)" -Subject "$($AuditLogFieldChanges) for $($StartDate) on $($ComputerName)" -Body "Generated $($AuditLogFieldChanges) report on $($ComputerName) for $($StartDate)." -Attachment "$($WorkingDirectory)\Reports\$($AuditLogFcFileName)" -SmtpServer mx01.minedata.no -Port 25
+               if ($?){
+                  Write-Log "Email sent to $($ToEmailAddress) ..."
+               }
+         }
+      } else {
+         Write-Log "Report file was not found."
+      }
    }
 }
 
+# Get the genereated file details for TimeSpent report.
+$TimeSpentFileName, $TimeSpentFileNames, $TimeSpentFileNameCount = Get-TimeSpentFileName
 
-#cmd /c $CMD_TimeSpent
+if ($TimeSpentFileNameCount -gt 0) {
 
+   Write-Log "File $($TimeSpentFileName) already exists ... Starting to send emails."
+
+   # Send email with AuditLogFieldchanges report attached.
+   foreach ($ToEmailAddress in $ToEmailAddressesList) {
+      Send-MailMessage -From "no-reply@navitro.com" -To "$($ToEmailAddress)" -Subject "$($TimeSpent) for $($StartDate) on $($ComputerName)" -Body "Generated $($TimeSpent) report on $($ComputerName) for $($StartDate)." -Attachment "$($WorkingDirectory)\Reports\$($TimeSpentFileName)" -SmtpServer mx01.minedata.no -Port 25
+      if ($?){
+         Write-Log "Email sent to $($ToEmailAddress) ..."
+      }
+   }
+} else {
+
+   Write-Log "File not found ... running command to generate the file."
+
+   # Run the ManagementConsole using commandprompt uses the above variables which makes the command.   
+   cmd /c $CMD_TimeSpent
+
+   if ($?){
+      #Get the generated file name.
+      $TimeSpentFileName, $TimeSpentFileNames, $TimeSpentFileNameCount = Get-TimeSpentFileName
+
+      if (Test-Path $WorkingDirectory\Reports\$TimeSpentFileName){
+         Write-Log "File Generated ... $($TimeSpentFileName)"
+         
+         # Send email with AuditLogFieldchanges report attached.
+         foreach ($ToEmailAddress in $ToEmailAddressesList) {
+            Send-MailMessage -From "no-reply@navitro.com" -To "$($ToEmailAddress)" -Subject "$($TimeSpent) for $($StartDate) on $($ComputerName)" -Body "Generated $($TimeSpent) report on $($ComputerName) for $($StartDate)." -Attachment "$($WorkingDirectory)\Reports\$($TimeSpentFileName)" -SmtpServer mx01.minedata.no -Port 25
+            if ($?){
+               Write-Log "Email sent to $($ToEmailAddress) ..."
+            }
+         }
+      } else {
+         Write-Log "Report file was not found."
+      }
+   }
+}
+
+if ($?){
+   Write-Log "Operation completed successfully ..."
+
+   foreach ($ErrorMail in $ErrorMailgList){
+      Send-MailMessage -From "no-reply@navitro.com" -To "$($ErrorMail)" -Subject "CAP Auto Report Script" -Body "Report genration successfully ended on the $($ComputerName) server at $($(Get-Date).ToString("HH:mm:ss")) on $($(Get-Date).ToString("dd.MM.yyyy"))." -SmtpServer mx01.minedata.no -Port 25
+   }
+
+   # Terminate the script. If generation is successfull.
+   [Environment]::Exit(0)
+} else {
+   Write-Log "Somthing went wrong. Did not complete properly ..."
+
+   foreach ($ErrorMail in $ErrorMailgList){
+      Send-MailMessage -From "no-reply@navitro.com" -To "$($ErrorMail)" -Subject "CAP Auto Report Script" -Body "Report genration not successfull on the server $($ComputerName). Something went wrong." -SmtpServer mx01.minedata.no -Port 25
+   }
+
+   # Terminate the script. If generation is unsuccessfull.
+   [Environment]::Exit(1)
+}
