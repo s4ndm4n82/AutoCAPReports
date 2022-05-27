3820a607069a069314444a79db6c7575c675e6ce
s4ndm4n82 <sadaruwan12@gmail.com>
2022-05-27 16:09:07 +0530

    First non tested code.

--- IndividualReports.ps1
+++ IndividualReports.ps1
@@ -37,3 +37,119 @@
 #  "C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport ItemsPerJob %FromDate% %ToDate% "C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL
 #  "C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport TimeSpent %FromDate% %ToDate% "C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL
 #  "C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport AuditLogFieldChanges %FromDate% %ToDate% "C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL /fields:"Voucher Type,Payment Method,Currency,Voucher Date"
+
+# Variable Assignment #
+
+# Menu text.
+# Top banner.
+$NewLine = [Environment]::Newline
+
+$Banner += "            ******************************************************
+            *                                                    *
+            *              Manual Reports Generator              *
+            *                                                    *
+            *  Created by: Sandaruwan Samaraweera                *
+            *  Version: 1.0.1                                    *
+            *                                                    *
+            ******************************************************$($NewLine)
+---------------------------------------------------------------------------------$($NewLine)"
+
+$TableSelectOptions +="       Select the Server.$($NewLine)
+        1. Navitro - 02 (Old server.)
+        2. DCProd - 01 (New server.)
+        x. Exit.$($NewLine)"
+
+# Working directory.
+#$WorkingDirectory = 'C:\Tools\Reports'
+$WorkingDirectory = 'F:\Users\SiNUX\MEGA\GitRepoShare\Repos\AutoCAPReports'
+
+# Dates.
+$StartDate = ""
+$EndDate = ""
+
+# Command.
+$McCommand = '"C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport'
+
+# Database.
+$DataBaseAddress    = ''
+$DataBaseTableV1    = ''
+$DataBaseTableV2    = ''
+$DataBaseUser     = ''
+$DataBasePassword = ''
+
+# Report output type.
+$ReportOutPdf   = '/type:PDF'
+$ReportOutExl   = '/type:EXCEL'
+$ReportOutWrd   = '/type:WORD'
+
+# Used field.
+$UsedField = '/fields:"Voucher Type"'
+
+# Report names.
+$ItemsPerJob                = 'ItemPerJob'
+$ItemsPerJobDetailed        = 'ItemsPerJobDetailed'
+$ImportedExported           = 'ImportedExported'
+$GroupByFields              = 'GroupByFields'
+$GroupByFieldsJobColumns    = 'GroupByFieldsJobColumns'
+$ProcessingTime             = 'GroupByFieldsJobColumns'
+$AuditLogFieldChanges       = 'AuditLogFieldChanges'
+$AuditLogCount              = 'AuditLogCount'
+$TimeSpent                  = 'TimeSpent'
+
+# Report output folders.
+$ItemsPerJobFolder                = 'ItemPerJob'
+$ItemsPerJobDetailedFolder        = 'ItemsPerJobDetailed'
+$ImportedExportedFolder           = 'ImportedExported'
+$GroupByFieldsFolder              = 'GroupByFields'
+$GroupByFieldsJobColumnsFolder    = 'GroupByFieldsJobColumns'
+$ProcessingTimeFolder             = 'GroupByFieldsJobColumns'
+$AuditLogFieldChangesFolder       = 'AuditLogFieldChanges'
+$AuditLogCountFolder              = 'AuditLogCount'
+$TimeSpentFolder                  = 'TimeSpent'
+
+# Folder name array.
+$OutputFolderNameArray  = @($ItemsPerJobFolder, $ItemsPerJobDetailedFolder, $ImportedExportedFolder, $GroupByFieldsFolder
+                            , $GroupByFieldsJobColumnsFolder, $ProcessingTimeFolder, $AuditLogFieldChangesFolder
+                            , $AuditLogCountFolder, $TimeSpentFolder)
+
+# Output folder path.
+$ReportMainFolderPath   = "$($WorkingDirectory)\Reports"
+
+function CheckFolders() {
+    # Output folder check.
+    if (!(Test-Path $ReportMainFolderPath)){
+        New-Item -ItemType Directory -Path $ReportMainFolderPath | Out-Null
+    }
+
+    foreach ($OutputFolderName in $OutputFolderNameArray){
+        $OutputFolderPathFull = "$($ReportMainFolderPath)\$($OutputFolderName)"
+
+        if (!(Test-Path $OutputFolderPathFull)){
+            New-Item -ItemType Directory -Path $OutputFolderPathFull | Out-Null
+        }
+    }
+}
+
+function MenuDatabase() {
+    Write-Output "$($Banner)"
+    Write-Output "$($TableSelectOptions)"
+}
+
+function MakeReportNavitro02() {
+    Write-Output "Navitro 02 selected."
+}
+
+function MakeReportDCProd01() {
+    Clear-Host
+    Write-Output "DCProd 01 selected."
+}
+
+do {
+    MenuDatabase
+    $UserServerSelect = Read-Host "       You selection: "
+
+    switch ($UserServerSelect) {
+        '1' {MakeReportNavitro02}
+        '2' {MakeReportDCProd01}
+    }
+} until (($UserServerSelect -eq 'x') -or ($UserServerSelect -eq '1') -or ($UserServerSelect -eq '2'))
