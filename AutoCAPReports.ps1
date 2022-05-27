aa03ac2510aeb9929e1b4694d9875e910c643390
s4ndm4n82 <sadaruwan12@gmail.com>
2022-05-27 12:51:56 +0530

    Fixed a command mistake.

--- AutoCAPReports.ps1
+++ AutoCAPReports.ps1
@@ -38,14 +38,6 @@
 #  "C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport TimeSpent %FromDate% %ToDate% "C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL
 #  "C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport AuditLogFieldChanges %FromDate% %ToDate% "C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL /fields:"Voucher Type,Payment Method,Currency,Voucher Date"
 
-<#
-$cmd_command = '"C:\Program Files (x86)\Navitro\CAP\ManagementConsole.exe" /CreateReport AuditLogFieldChanges'
-$dates =  $from_date +" "+ $to_date
-$pm = '"C:\Reports" dmz-sql-01.dmz.local\pronav,59116 ATC_Archive sa Asi@16 /type:EXCEL /fields:"Voucher Type,Payment Method,Currency,Voucher Date"'
-
-$cmd = $cmd_command + " " + $dates + " " + $pm
-#>
-
 # Variable decleration.
 #Directory locations
 $WorkingDirectory = "E:\Tools\AutoCAPReports" # Directory location of the script. Please set this path if not script wont work corrrectly.
